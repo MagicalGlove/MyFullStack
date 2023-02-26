c4038f2d7f4e302c707b66bd48a094b7271e5654
Magical Glove <90389752+MagicalGlove@users.noreply.github.com>
2023-02-26 19:06:45 +0100

    Styles the table with people and adds occupation

--- week4_ts2/week4exercises/src/App.tsx
+++ week4_ts2/week4exercises/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 //Json endpoints are premade. Link/person post, link/person get, link/person/id put, link/person
+//To run json-server locally (Be in the right folder): npx json-server --watch people.json --port 3001
 
 function DisplayName(props: any) {
     return <div>
@@ -25,13 +26,14 @@ function PeopleViewer(): ReactElement {
     type Person = { id: number, name: string, age: number, city: string }
 
     useEffect(() => {
-        fetch("http://localhost:3008/person")
+        fetch("http://localhost:3001/person")
             .then((res) => res.json())
             .then((res) => setPerson(res))
 
     }, [])
 
     return <div>
+
         <table>
             <thead>
             <th>Id</th>
